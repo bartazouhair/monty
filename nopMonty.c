1e86b4097b56c9df73ce935a6a2f021d555bdcd6
HichamELMaa <hm.elmaataoui@gmail.com>
2023-06-22 03:24:19 -0700

    Nop monty

---
+++ nopMonty.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nopMonty - Does nothing
+ * @stk: Pointer to the stack head
+ * @cntr: Line number
+ *
+ * Return: void
+ */
+void nopMonty(stack_t **stk, unsigned int cntr)
+{
+	(void)cntr;
+	(void)stk;
+}
