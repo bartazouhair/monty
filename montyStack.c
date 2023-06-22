715037d4042efe44f6e8dd6133861c6516c5933d
HichamELMaa <hm.elmaataoui@gmail.com>
2023-06-22 03:22:04 -0700

    monty stacks

---
+++ montyStack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * montyStack - Initializes the stack for Monty interpreter
+ * @stk: Pointer to the stack
+ * @cntr: Line number
+ *
+ * Return: void
+ */
+void montyStack(stack_t **stk, unsigned int cntr)
+{
+	(void)stk;
+	(void)cntr;
+	bs.li = 0;
+}
