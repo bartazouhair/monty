57a05a9c6e84f69978f428be8c48d8500cac9c71
bartazouhair <bartazouhair@gmail.com>
2023-06-22 04:58:39 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+bus_y bs = {NULL, NULL, NULL, 0};
+
+/**
+ * main - to monty code interpreter glob
+ * @argc: it's number of arguments
+ * @argv: it's monty file location
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	char *cont;
+	FILE *f;
+	size_t sz = 0;
+	ssize_t rl = 1;
+	stack_t *stk = NULL;
+	unsigned int cntr = 0;
+
+	if (argc != 2)
+	{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+	}
+	f = fopen(argv[1], "r");
+	bs.fl = f;
+	if (!f)
+	{
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+	exit(EXIT_FAILURE);
+	}
+	while (rl > 0)
+	{
+	cont = NULL;
+	rl = getline(&cont, &sz, f);
+	bs.cnt = cont;
+	cntr++;
+	if (rl > 0)
+	{
+		executeMonty(cont, &stk, cntr, f);
+	}
+	free(cont);
+	}
+	freeMontyStack(stk);
+	fclose(f);
+	return (0);
+}
